26b772bc357d3ac4e11aa7b03350855dfa32d6b4
eliza <twew02@gmail.com>
2022-11-13 17:30:21 -0500

    setup dropdown feature

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is javascript front end library.'
+    title: "What is React?",
+    content: "React is javascript front end library.",
   },
   {
-    title: 'Why use React?',
-    content: 'React is popular and used by many developers.'
+    title: "Why use React?",
+    content: "React is popular and used by many developers.",
   },
   {
-    title: 'Who created React?',
-    content: 'A team of developers from Facebook.'
-  }
-]
+    title: "Who created React?",
+    content: "A team of developers from Facebook.",
+  },
+];
+
+const options = [
+  {
+    label: "The color of red",
+    value: "red",
+  },
+  {
+    label: "The color of green",
+    value: "green",
+  },
+  {
+    label: "The color of blue",
+    value: "blue",
+  },
+];
 
 const App = () => {
-  
   return (
     <div>
-     <Search />
-      {/* { <Accordion items={items}/> /*name of prop is items and the value is the items array */} 
+      {/* <Search /> */}
+      {/* { <Accordion items={items}/> /*name of prop is items and the value is the items array */}
+      <Dropdown options={options} />
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -21,12 +21,20 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-    const timeoutId = setTimeout(()=>{
-      if (term) {
-        search();
-      }
-    },500)
-      
+
+    if(term && !results.length){
+      search()
+    } else {
+      const timeoutId = setTimeout(()=>{
+        if (term) {
+          search();
+        }
+      },1000)
+        return () => {
+          clearTimeout(timeoutId);
+        };
+    }
+   
   }, [term]);
 
   const renderedResults = results.map((result) => {
@@ -65,3 +73,5 @@ const Search = () => {
   );
 };
 export default Search;
+
+
